339c98bc1d871e6d633789d0af9ee9f7c25229c6
Khalyomede <khalyomede@users.noreply.github.com>
2023-01-12 22:25:07 +0100

    #3 add ability to create listing from array (#5)
    * #3 add ability to create listing from array
    
    * #3 add PHP dev dependencies updates

--- tests/ReorderTest.php
+++ tests/ReorderTest.php
@@ -139,3 +139,27 @@ test("can find object item", function (): void {
 
     expect($list->find($tables)->order)->toBe(2);
 });
+
+test("can create a listing from an array", function (): void {
+    $listing = Listing::from([
+        new Item("bag", 1),
+        new Item("book", 2),
+        new Item("chair", 3),
+        new Item("table", 4),
+    ]);
+
+    expect($listing->find("bag")->order)->toBe(1);
+    expect($listing->find("book")->order)->toBe(2);
+    expect($listing->find("chair")->order)->toBe(3);
+    expect($listing->find("table")->order)->toBe(4);
+});
+
+test("throws an exception if creating a listing from an array of non Item", function (): void {
+    /** @phpstan-ignore-next-line */
+    expect(fn (): Listing => Listing::from([
+        ["bag", 1],
+        ["book", 2],
+        ["chair", 3],
+        ["table", 4],
+    ]))->toThrow(TypeError::class, 'Khalyomede\ReorderBeforeAfter\Listing::Khalyomede\ReorderBeforeAfter\{closure}(): Argument #1 ($item) must be of type Khalyomede\ReorderBeforeAfter\Item, array given');
+});
